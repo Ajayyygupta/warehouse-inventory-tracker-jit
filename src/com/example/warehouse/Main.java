5f54a37f0144347617076a4b62c9ae7020edf8c8
Ajay Gupta <ajju211503@gmail.com>
2025-10-30 08:59:46 -0700

    Dynamic update logic added

--- src/com/example/warehouse/Main.java
+++ src/com/example/warehouse/Main.java
@@ -1,19 +1,76 @@
 package com.example.warehouse;
 
+import java.util.Scanner;
+
+
 public class Main {
 	
 	public static void main(String[] args) {
 		
-		AlertService alertService=new RestockAlertService();
-		Warehouse w1=new Warehouse(alertService);
+		Scanner sc=new Scanner(System.in);
+		AlertService alertService=new RestockAlertService(); 
+		Warehouse warehouse=new Warehouse(alertService);
 		
-		  w1.addProduct(new Product("P101", "Laptop", 0, 5));
-	        w1.receiveShipment("P101", 10); 
-	        w1.fulfillOrder("P101", 6);    
-	        w1.showInventory();
+		  int choice;
+	        do {
+	            System.out.println("\n WAREHOUSE INVENTORY TRACKER");
+	            System.out.println("1.Add Product");
+	            System.out.println("2.Receive Shipment");
+	            System.out.println("3.Fulfill Order");
+	            System.out.println("4.Show Inventory");
+	            System.out.println("5.Exit");
+	            System.out.print("Enter your choice: ");
+	            choice = sc.nextInt();
+	            sc.nextLine();
+	            
+	            switch (choice) {
+				case 1:
+					System.out.println("ENter Product ID");
+					String id=sc.nextLine();
+					
+					System.out.println("ENter Product Name");
+					String name=sc.nextLine();
+					
+					System.out.println("ENter Quantity");
+					Integer quantity=sc.nextInt();
+					
+					System.out.println("ENter Threshold");
+					Integer thresold=sc.nextInt();
+					
+					Product productItem=new Product(id, name, quantity, thresold);
+					warehouse.addProduct(productItem);
+					break;
+					
+				case 2:
+					System.out.println("ENter Product ID for Shipment");
+					String sid=sc.nextLine();
+					System.out.println("ENter Quantity to ADD");
+					int addQty=sc.nextInt();
+					warehouse.receiveShipment(sid, addQty);
+					break;
+					
+				case 3:
+					System.out.println("ENter Product ID for Order");
+					String oid=sc.nextLine();
+					System.out.println("ENter Quantity to fulfill");
+					int orderQty=sc.nextInt();
+					warehouse.fulfillOrder(oid, orderQty);
+					break;
+					
+				case 4:
+					warehouse.showInventory();
+					break;
+					
+				case 5:
+					System.out.println("Existing! Thankyou");
+					break;
+					
+				default:
+					System.out.println("Invvalidd choise try again");
+	            }
+	         
+	        } while(choice !=5);
+	        sc.close();
 		
 	}
-	
-	
-
 }
